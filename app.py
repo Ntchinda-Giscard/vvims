f4089dc399ec82565191072b02a41d01e0c63ed2
Ntchinda Giscard <ntchindagiscard@MacBook-Pro-de-Aurelien.local>
2024-05-09 14:57:17 +0100

    add welcome screen and describtion

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
-from pyparsing import html_comment
-from starlette.responses import HTMLResponse
+from fastapi.responses import HTMLResponse
 import os
 
 from utils import licence_dect, ner_recog, read_text_img
@@ -14,11 +13,61 @@ UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/", response_class=HTMLResponse)
-async def index():
+async def read_items():
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Welcome to My App</title>
+        <style>
+        .text {
+            font-family: Arial, sans-serif;
+            font-size: 16px;
+            font-weight: bold;
+            color: #333;
+            line-height: 1.5;
+            text-align: center;
+            text-decoration: none;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            word-spacing: 2px;
+            border: 2px solid #ccc;
+            border-radius: 18px;
+            padding: 25px;
+            }
 
-    return HTMLResponse(content=html_comment)
+            body {
+                background-color: #f0f0f0;
+                font-family: Arial, sans-serif;
+            }
+            .container {
+                max-width: 800px;
+                margin: 0 auto;
+                padding: 20px;
+                text-align: center;
+            }
+            .footer {
+                margin-top: 50px;
+                font-size: 14px;
+                color: #666;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Welcome to VVIMS AI App! 😊</h1>
+            <p class="text"> Explore the wonders of our OCR and ANPR APIs! These powerful tools utilize AI to effortlessly decipher and recognize elements within Cameroonian ID cards, extracting valuable information with just a simple call to the <code> "/idextract" </code> endpoint. With our technology, you'll gain the ability to see beyond the surface and effortlessly identify vehicle license plates using the <code>"/carplate"</code> endpoint. The power is now yours to wield. Unleash the full potential of these tools and revolutionize your workflow..</p>
+            <p>Let this app be the beginning of your journey towards greatness!</p>
+        </div>
+        <div class="footer">
+            <p>Made with ❤️ by Ntchinda Giscard</p>
+        </div>
+    </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
-@app.post("/idextract")
+@app.post("/idextract", description="This endpoint expects two files one named front and the other back which corresponds to the front and back of the id card, and returns a list of with entity_back and entity_front being the extracted infomation from the image")
 async def upload_files(front: UploadFile = File(...), back: UploadFile = File(...)):
     """
     Endpoint to receive front and back image uploads and save them to disk.
@@ -61,7 +110,7 @@ async def upload_files(front: UploadFile = File(...), back: UploadFile = File(..
     except Exception as e:
         return {"message": f"Internal server error: {str(e)}", "status_code": 500}
 
-@app.post("/carplate")
+@app.post("/carplate", description="This endpoint expects a file named license and return a list of turple having the detected number plates and the extracted text")
 async def carplate(license: UploadFile = File(...)):
     img_path = 'uploads/car.jpg'
     try:
