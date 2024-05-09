537228d39a304c67cc274c5a61609350aa2d7818
Ntchinda Giscard <ntchindagiscard@MacBook-Pro-de-Aurelien.local>
2024-05-09 12:15:15 +0100

    cropped image

--- utils.py
+++ utils.py
@@ -1,10 +1,40 @@
 from paddleocr import PaddleOCR, draw_ocr
 import spacy
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 
 ocr_model = PaddleOCR(lang='en')
 nlp_ner = spacy.load("output/model-best")
+detector = YOLO('best.pt')
 
 
+def licence_dect(img: any) -> list:
+    image = Image.open(img)
+    # image = np.array(image)
+    results = detector(img)
+    detections = []
+    boxes = results[0].numpy() 
+    print(boxes)
+    for result in results:
+        x1, y1, x2, y2, confidence, _ = result[0].numpy()
+
+        confidence = float(confidence)
+        print("Confidence :", confidence)
+        print("Confidence :", confidence)
+
+        cropped_image = image.crop((x1, y1, x2, y2))
+
+        plt.imshow(cropped_image)
+        plt.show()
+
+        detections.append(result)
+    
+    return detections
+
+res = licence_dect("IMG_0551.JPG" )
+print(res)
 def ner_recog(text:str) -> dict:
     """
     Extract entities from text using SpaCy and return them as JSON.
@@ -45,4 +75,3 @@ def read_text_img(img_path:str) -> str:
     for res in result[0]:
         text += res[1][0] + ' '
     return text
-  # print(res[1][0])
