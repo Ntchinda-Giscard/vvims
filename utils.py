1111c9f01cfe740c04259492e10a25c361e1c6c4
Ntchinda-Giscard <ntchinda1998@gmail.com>
2024-05-25 07:17:30 +0100

    remove secret

--- utils.py
+++ utils.py
@@ -26,13 +26,13 @@ bucket_name='vvims'
 def upload_to_s3(
         file_path, 
         bucket_name=bucket_name, 
-        aws_access_key_id=aws_access_key_id, 
-        aws_secret_access_key=aws_secret_access_key,
+        # aws_access_key_id=aws_access_key_id, 
+        # aws_secret_access_key=aws_secret_access_key,
         region_name='eu-north-1'):
     # Create an S3 client
     s3 = boto3.client('s3', 
-                      aws_access_key_id=aws_access_key_id,
-                      aws_secret_access_key=aws_secret_access_key,
+                    #   aws_access_key_id=aws_access_key_id,
+                    #   aws_secret_access_key=aws_secret_access_key,
                       region_name=region_name)
     
     # Get the current timestamp
